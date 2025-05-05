40b0c9ddc472d2f7938ae236d4cdcd9cee1e6626
redpanda19 <postche34@gmail.com>
2025-05-05 19:15:00 -0400

    Update fxmanifest.lua

--- ndrp-vehiclespeed/fxmanifest.lua
+++ ndrp-vehiclespeed/fxmanifest.lua
@@ -1,9 +1,7 @@
-
 fx_version 'cerulean'
 game 'gta5'
-author 'redpanda19'
 description 'Speed Limiter with Custom Vanilla Handling'
-version '0.0.1'
+version '5.0.0'
 lua54 'yes'
 
 client_script 'vehicle_speed.lua'
@@ -31,12 +29,10 @@ data_file 'HANDLING_FILE' 'car-classes/S-Class.meta'
 data_file 'HANDLING_FILE' 'car-classes/I-Class.meta'
 data_file 'HANDLING_FILE' 'car-classes/LF-Class.meta'
 data_file 'HANDLING_FILE' 'car-classes/U-Class.meta'
-data_file 'HANDLING_FILE' 'car-classes/V-Class.meta'
 data_file 'HANDLING_FILE' 'car-classes/M-Class.meta'
 data_file 'HANDLING_FILE' 'car-classes/O-Class.meta'
 data_file 'HANDLING_FILE' 'car-classes/P-Class.meta'
 data_file 'HANDLING_FILE' 'car-classes/DR-Class.meta'
+
 resource_type 'handling'
-
-
 dependency '/assetpacks'
